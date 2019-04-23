96a0bbcd533e1938fe086e687192d9e1f1e798dc
Brent Irwin <brent@brentirwin.tv>
2019-04-23 11:05:16 -0500

    Updates

---
+++ precache-manifest.e4620878773865cb91a68a65ecfaa749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7946050466da5cf173f8",
+    "url": "/sunshine-index/static/css/main.d57fc953.chunk.css"
+  },
+  {
+    "revision": "7946050466da5cf173f8",
+    "url": "/sunshine-index/static/js/main.26027bd0.chunk.js"
+  },
+  {
+    "revision": "f9408b5940f0dc76a863",
+    "url": "/sunshine-index/static/js/runtime~main.0c2b5b96.js"
+  },
+  {
+    "revision": "10feab1a40953389f59f",
+    "url": "/sunshine-index/static/js/2.519dea31.chunk.js"
+  },
+  {
+    "revision": "80edb98169db765613f77c238f680023",
+    "url": "/sunshine-index/static/media/darksky.80edb981.png"
+  },
+  {
+    "revision": "323a778a318022aa0bb42610e20e9db9",
+    "url": "/sunshine-index/index.html"
+  }
+];
